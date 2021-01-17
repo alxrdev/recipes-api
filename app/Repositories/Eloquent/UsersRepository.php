0ed6cae3d84439b6925702e0a792433fbe79fcc1
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-16 22:55:47 -0300

    UsersRepository has been moved to Eloquent folder

--- app/Repositories/Eloquent/UsersRepository.php
+++ app/Repositories/Eloquent/UsersRepository.php
@@ -1,12 +1,22 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Repositories\Eloquent;
 
 use App\Models\User;
 use App\Repositories\Interfaces\IUsersRepository;
 
 class UsersRepository implements IUsersRepository
 {
+    /**
+     * @var User $user
+     */
+    private User $user;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
     /**
      * Method that creates a new user
      * 

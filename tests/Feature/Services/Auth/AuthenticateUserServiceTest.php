beb6579c61e09032e40454e8a9511a4317113238
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-18 12:15:03 -0300

    Test if the email and password do not match

--- tests/Feature/Services/Auth/AuthenticateUserServiceTest.php
+++ tests/Feature/Services/Auth/AuthenticateUserServiceTest.php
@@ -2,30 +2,47 @@
 
 namespace Tests\Feature\Services\Users;
 
+use App\Exceptions\AppException;
 use App\Models\User;
 use App\Services\Auth\Interfaces\IAuthenticateUserService;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthenticateUserServiceTest extends TestCase
 {
-    use DatabaseMigrations, RefreshDatabase;
+    use DatabaseMigrations;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        User::factory()->create([
+            'email' => 'user@gmail.com'
+        ]);
+    }
 
     /**
      * @test
      */
     public function should_return_a_token()
     {
-        User::factory()->create([
-            'email' => 'user@gmail.com'
-        ]);
-
         $authenticateUserService = app(IAuthenticateUserService::class);
 
         $token = $authenticateUserService->execute(['email' => 'user@gmail.com', 'password' => 'password']);
 
         $this->assertNotEmpty($token);
         $this->assertIsString($token);
+    }
+
+    /**
+     * @test
+     */
+    public function should_throw_an_AppException_if_the_email_and_password_do_not_match()
+    {
+        $authenticateUserService = app(IAuthenticateUserService::class);
+
+        $this->expectException(AppException::class);
+        
+        $authenticateUserService->execute(['email' => 'user@gmail.com', 'password' => 'invalid']);
     }
 }

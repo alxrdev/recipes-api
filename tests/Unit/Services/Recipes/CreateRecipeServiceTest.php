716bb8e784bc33cb1bde9283baa2d167c4e97bc9
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-20 10:52:30 -0300

    Test has been refactored

--- tests/Unit/Services/Recipes/CreateRecipeServiceTest.php
+++ tests/Unit/Services/Recipes/CreateRecipeServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Services\Recipes;
 use App\Models\Recipe;
 use App\Repositories\Interfaces\IRecipesRepository;
 use App\Services\Recipes\Interfaces\ICreateRecipeService;
+use Illuminate\Http\UploadedFile;
 use Mockery;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -32,15 +33,18 @@ class CreateRecipeServiceTest extends TestCase
     {
         $createRecipeService = app(ICreateRecipeService::class);
 
-        $recipe = $createRecipeService->execute([
+        $recipe = $createRecipeService->execute(
+        [
             'user_id' => 1,
             'title' => 'My recipe',
             'description' => 'This is my recipe',
-            'image' => 'recipe-image.jpg',
             'preparation_time' => '00:30:00',
             'ingredients' => 'ingredient1::ingredient1::ingredient3::ingredient4',
-            'steps' => '[{position:1,image:"step1.jpg",content:"this is the step 1"},{position:2,image:"step2.jpg",content:"this is the step 2"}]',
+            'steps' => '[{"position":1,"image":"step1.jpg","content":"this is the step 1"},{"position":2,"image":"step2.jpg","content":"this is the step 2"}]',
             'difficulty' => 5
+        ],
+        [
+            'image' => UploadedFile::fake()->create('image.jpg')
         ]);
 
         $this->assertInstanceOf(Recipe::class, $recipe);

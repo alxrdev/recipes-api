09a0b26a1e6151c50b4d84e1064a595e920e26c3
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-19 21:10:36 -0300

    Test the CreateRecipeService

---
+++ tests/Unit/Services/Recipes/CreateRecipeServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\Services\Recipes;
+
+use App\Models\Recipe;
+use App\Repositories\Interfaces\IRecipesRepository;
+use App\Services\Recipes\Interfaces\ICreateRecipeService;
+use Mockery;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class CreateRecipeServiceTest extends TestCase
+{
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->instance(
+            IRecipesRepository::class,
+            Mockery::mock(IRecipesRepository::class, function (MockInterface $mock) {
+                $mock->shouldReceive('store')
+                    ->once()
+                    ->andReturn(new Recipe());
+            }
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function should_return_the_created_recipe()
+    {
+        $createRecipeService = app(ICreateRecipeService::class);
+
+        $recipe = $createRecipeService->execute([
+            'user_id' => 1,
+            'title' => 'My recipe',
+            'description' => 'This is my recipe',
+            'image' => 'recipe-image.jpg',
+            'preparation_time' => '00:30:00',
+            'ingredients' => 'ingredient1::ingredient1::ingredient3::ingredient4',
+            'steps' => '[{position:1,image:"step1.jpg",content:"this is the step 1"},{position:2,image:"step2.jpg",content:"this is the step 2"}]',
+            'difficulty' => 5
+        ]);
+
+        $this->assertInstanceOf(Recipe::class, $recipe);
+    }
+}

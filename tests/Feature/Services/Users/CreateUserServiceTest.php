5b5ef62971d8468ff648ee53383ac972df1076b7
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-01-26 12:23:40 -0300

    CreateUserServiceTest has been refactored

--- tests/Feature/Services/Users/CreateUserServiceTest.php
+++ tests/Feature/Services/Users/CreateUserServiceTest.php
@@ -6,30 +6,35 @@ use App\Exceptions\AppException;
 use App\Models\User;
 use App\Repositories\Interfaces\IUsersRepository;
 use App\Services\Users\Interfaces\ICreateUserService;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Mockery;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class CreateUserServiceTest extends TestCase
 {
-    use DatabaseMigrations;
-
     /**
      * @test
      */
     public function should_create_a_new_user()
     {
-        $usersRepository = app(IUsersRepository::class);
+        $this->instance(
+            IUsersRepository::class,
+            Mockery::mock(IUsersRepository::class, function (MockInterface $mock) {
+                $mock->shouldReceive('create')
+                    ->once()
+                    ->andReturn(new User());
+            }
+        ));
+
         $createUserService = app(ICreateUserService::class);
         
-        $createUserService->execute([
+        $user = $createUserService->execute([
             'name' => 'Alex Rodrigues Moreira', 
             'email' => 'user@gmail.com', 
             'password' => 'mypassword'
         ]);
-        
-        $createdUser = $usersRepository->getByEmail('user@gmail.com');
 
-        $this->assertInstanceOf(User::class, $createdUser);
+        $this->assertInstanceOf(User::class, $user);
     }
 
     /**
@@ -37,16 +42,19 @@ class CreateUserServiceTest extends TestCase
      */
     public function should_throw_an_AppException_when_a_user_with_the_same_email_already_exists()
     {
+        $this->instance(
+            IUsersRepository::class,
+            Mockery::mock(IUsersRepository::class, function (MockInterface $mock) {
+                $mock->shouldReceive('create')
+                    ->once()
+                    ->andThrow(new AppException('The given data was invalid.', ['email' => 'The email has already been taken.'], 400));
+            }
+        ));
+
         $createUserService = app(ICreateUserService::class);
 
         $this->expectException(AppException::class);
         
-        $createUserService->execute([
-            'name' => 'Alex Rodrigues Moreira', 
-            'email' => 'user@gmail.com', 
-            'password' => 'mypassword'
-        ]);
-
         $createUserService->execute([
             'name' => 'Alex Rodrigues Moreira', 
             'email' => 'user@gmail.com', 
